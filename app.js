f73bc3877cb9c274ca513cb181f47dc57e752257
Ashmeet670 <73339721+Ashmeet670@users.noreply.github.com>
2024-10-20 22:21:54 +0530

    input text box adn button click

--- app.js
+++ app.js
@@ -8,3 +8,16 @@ function translationModeChange(btn){
     translateMode = btn.id
     console.log(translateMode)
 }
+
+function buttonClick(btn){
+    btn.classList.add("buttonClickAnimate")
+
+    setTimeout(() => {
+        btn.classList.remove("buttonClickAnimate")
+    }, 400);
+}
+
+
+function translates(){
+    console.log("e")
+}
